9f56732cc45500f57658b00dad78d7f593acf257
shintaro.sakata <shintaro.sakata.tokyo@gmail.com>
2019-12-04 16:03:21 +0900

    add sample_project

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use next_parser;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    let input = "
-    fn main() {
-        let x = 114514;
-        return x;
-    }
-    ";
+    let args: Vec<String> = env::args().collect();
+    let filename = if args.len() >= 2 {
+        &args[1]
+    } else {
+        "sample_project/main.nx"
+    };
 
-    match next_parser::parse(input) {
+    let mut f = File::open(filename).expect(&format!("{} file not found", filename));
+
+    let mut input = String::new();
+    f.read_to_string(&mut input)
+        // ファイルの読み込み中に問題がありました
+        .expect("something went wrong reading the file");
+
+    match next_parser::parse(&input) {
         Ok(v) => println!("{:?}", v),
         Err(e) => println!("{:?}", e),
     };
